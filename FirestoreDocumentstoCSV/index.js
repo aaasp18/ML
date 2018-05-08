6d80a317844169323c82722eaf4f606db8725b3f
akkisho <akkisho@amazon.com>
2018-05-08 23:03:11 +0530

    fix MAC filtering

--- FirestoreDocumentstoCSV/index.js
+++ FirestoreDocumentstoCSV/index.js
@@ -15,10 +15,8 @@ const parser = new Json2csvParser();
 
 
 var features = new Set();
-var filter = new Set(['CDOT-MSRIT', 'Dr.MKN', 'CSE-LAB']);
-
+var filter = new Set(['*','CDOT-MSRIT', 'Dr.MKN', 'CSE-LAB']);
 var collection = [];
-
 var count = 0;
 
 var query = fingerprintsRef.get()
@@ -28,6 +26,7 @@ var query = fingerprintsRef.get()
         var id = doc.id;
         var docData = doc.data();
         var roomId = docData.roomId;
+
         docData.data.forEach( item => {
           count++;
           var entry = {};
@@ -35,18 +34,15 @@ var query = fingerprintsRef.get()
           item.fingerprint.forEach(fingerprint => {
             if((filter.has('*') || filter.has(fingerprint.ssid)) && !features.has(fingerprint.bssid)){
               features.add(fingerprint.bssid);
-              // entry[fingerprint.bssid] = fingerprint.level;
             }
-            entry[fingerprint.bssid] = fingerprint.level;
+            if(features.has(fingerprint.bssid)){
+              entry[fingerprint.bssid] = fingerprint.level;
+            }
           });
           collection.push(entry);
         });
       });
-      //console.log(features.size);
-      //console.log(features);
       console.log(count);
-      //console.log(collection.slice(Math.max(collection.length - 5, 1)));
-      //console.log(parser.parse(collection.slice(Math.max(collection.length - 5, 1))));
       fs.writeFile("data.csv", parser.parse(collection), function(err) {
         if(err) {
           return console.log(err);
